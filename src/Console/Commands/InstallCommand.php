0ff0b0b7c60c9dbf9c400da32db58b902418a8f6
Tii <mail@tii.one>
2023-03-08 17:02:55 +0100

    Fixed install Command on missing .env.example

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -34,26 +34,36 @@ class InstallCommand extends Command
     protected function setupEnvVariables()
     {
         $env = DotenvEditor::loadFile(base_path('.env'));
-        $example = DotenvEditor::loadFile(base_path('.env.example'));
 
         // Setup TELEGRAM_API_TOKEN
         if (! $env->get('TELEGRAM_API_TOKEN') || $this->option('force')) {
             $apiToken = $this->ask('If you already have a Bot API Token, enter it here');
             $env->set('TELEGRAM_API_TOKEN', $apiToken);
         }
-        if (! $example->has('TELEGRAM_API_TOKEN')) {
-            $example->set('TELEGRAM_API_TOKEN');
-        }
 
         // Setup TELEGRAM_WEBHOOK_SECRET
         if (! $env->has('TELEGRAM_WEBHOOK_SECRET') || $this->option('force')) {
             $env->set('TELEGRAM_WEBHOOK_SECRET', Str::random(32));
         }
+
+        $env->save();
+
+        if (! File::exists(base_path('.env.example'))) {
+            return;
+        }
+
+        $example = DotenvEditor::loadFile(base_path('.env.example'));
+
+        // Setup TELEGRAM_API_TOKEN
+        if (! $example->has('TELEGRAM_API_TOKEN')) {
+            $example->set('TELEGRAM_API_TOKEN');
+        }
+
+        // Setup TELEGRAM_WEBHOOK_SECRET
         if (! $example->has('TELEGRAM_WEBHOOK_SECRET')) {
             $example->set('TELEGRAM_WEBHOOK_SECRET');
         }
 
-        $env->save();
         $example->save();
     }
 
